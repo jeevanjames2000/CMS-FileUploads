e816e4c626b3f203d2abdbb14da97199ccfb5bfe
jeevan <jeevanjames2000@gmail.com>
2025-02-07 20:57:29 +0530

    delete,readme

--- routes/s3Bucket.js
+++ routes/s3Bucket.js
@@ -5,7 +5,7 @@ const AWSS3 = require("../controllers/awsS3Bucket");
 
 router.post("/uploadImage", AWSS3.uploadImages);
 router.get("/getImages", AWSS3.getImages);
-router.post("/deleteImages", AWSS3.deleteBulkImages);
+router.delete("/deleteImages", AWSS3.deleteBulkImages);
 router.delete("/deleteImage/:key", AWSS3.deleteImage);
 
 module.exports = router;
